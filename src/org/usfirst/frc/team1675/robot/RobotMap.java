02b4c4677ebbb03e78fec25065367da4cf307388
Josh Pordon <josh.pordon@gmail.com>
2017-01-14 09:34:30 -0600

    Initial repo commit

---
+++ src/org/usfirst/frc/team1675/robot/RobotMap.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team1675.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	public static class PWMChannels {
+
+	}
+	public static class CANDeviceIDs{
+		
+	}
+
+	public static class PDChannels {
+	}
+
+	public static class DIOChannels {
+
+	}
+
+	public static class AnalogInChannels {
+
+	}
+
+	public static class RelayChannels {
+	}
+
+	public static class DriverConstants {
+	}
+}
---
+++ src/org/usfirst/frc/team1675/robot/XBoxControllerMap.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team1675.robot;
+
+
+
+public class XBoxControllerMap {
+	
+
+
+	public static final int driverControllerPort = 0;
+	public static final int operatorControllerPort = 1;
+
+	// left analog stick
+	public static final int LEFT_X_AXIS = 0;
+	public static final int LEFT_Y_AXIS = 1;
+
+	// right analog stick
+	public static final int RIGHT_X_AXIS = 4;
+	public static final int RIGHT_Y_AXIS = 5;
+
+	// Trigger axis
+	// Starts at 0. Left trigger increases value, right trigger decreases value.
+	// NOTE that this means you cannot "detect" both triggers being pulled at
+	// any degree.
+	public static final int LEFT_TRIGGER_AXIS = 2;
+	public static final int RIGHT_TRIGGER_AXIS = 3;
+
+	// D-Pad horizontal axis.
+	// Starts at 0, goes down when left is pressed, up when right is pressed.
+	public static final int DPAD_AXIS = 6;
+
+	// Face Buttons
+	public static final int A_BUTTON = 1;
+	public static final int B_BUTTON = 2;
+	public static final int X_BUTTON = 3;
+	public static final int Y_BUTTON = 4;
+
+	// Bumpers (shoulders)
+	public static final int LEFT_BUMPER_BUTTON = 5;
+	public static final int RIGHT_BUMPER_BUTTON = 6;
+
+	// other buttons
+	public static final int BACK_BUTTON = 7;
+	public static final int START_BUTTON = 8;
+
+	// Joystick buttons (clicking them in)
+	public static final int LEFT_JOYSTICK_BUTTON = 9;
+	public static final int RIGHT_JOYSTICK_BUTTON = 10;
+
+	public static final double DEAD_ZONE = .1675;
+	
+
+}
