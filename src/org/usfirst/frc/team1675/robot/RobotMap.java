a710426f49bb1531f81a04b3c581a600f73d730e
GREEN KNIGHT <greenknight6969@team1675.com>
2017-01-26 18:53:19 -0600

    added shooter subsystem + constants/refactoring in robotmap (Jacob/Maverick/Nate)

--- src/org/usfirst/frc/team1675/robot/RobotMap.java
+++ src/org/usfirst/frc/team1675/robot/RobotMap.java
@@ -1,5 +1,4 @@
 package org.usfirst.frc.team1675.robot;
-
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -8,34 +7,28 @@ package org.usfirst.frc.team1675.robot;
  */
 public class RobotMap {
 	public static class PWMChannels {
-
 	}
-	public static class CANDeviceIDs{
-		//inaccurate info
+	public static class CANDeviceIDs {
+		// inaccurate info
 		public static final int LEFT_FRONT_MOTOR = 3;
 		public static final int LEFT_BACK_MOTOR = 0;
 		public static final int RIGHT_FRONT_MOTOR = 2;
 		public static final int RIGHT_BACK_MOTOR = 0;
+		public static final int SHOOTER_MOTOR = 0;
 	}
-
 	public static class PDChannels {
 	}
-
 	public static class DIOChannels {
-
 	}
-
 	public static class AnalogInChannels {
-
 	}
-
 	public static class RelayChannels {
 	}
-
 	public static class DriverConstants {
 	}
-	
-	public static class DriveBaseConstants{
-		public static final double MOTOR_DEADZONE = .1675;
+	public static class MotorConstants {
+		public static final double DRIVE_DEADZONE = .1675;
+		public static final double SHOOTER_DEADZONE = .1675;
+		public static final int ENCODER_TICKS_PER_ROTATION = 697;
 	}
 }
