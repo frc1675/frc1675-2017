a704f29316288eaaf5e9f42678f1ed1d31fa6456
GREEN KNIGHT <greenknight6969@team1675.com>
2017-01-26 20:17:53 -0600

    fixed shooter getRPM method and simplified its motor deadzone (Jacob)

--- src/org/usfirst/frc/team1675/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1675/robot/subsystems/Shooter.java
@@ -1,33 +1,37 @@
 package org.usfirst.frc.team1675.robot.subsystems;
+
 import org.usfirst.frc.team1675.robot.RobotMap;
 import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 /**
  *
  */
 public class Shooter extends Subsystem {
 	private CANTalon fireMotor;
-	
-	public Shooter(){
+
+	public Shooter() {
 		fireMotor = new CANTalon(RobotMap.CANDeviceIDs.SHOOTER_MOTOR);
 	}
-	public void setPower(double power){
+
+	public void setPower(double power) {
 		power = motorDeadzone(power);
 		fireMotor.set(power);
 	}
-	public double getSpeed(){
+
+	public double getSpeed() {
 		return fireMotor.getSpeed();
 	}
-	public double getRPM(){
-		return fireMotor.getEncVelocity() / RobotMap.MotorConstants.ENCODER_TICKS_PER_ROTATION;
+
+	public double getRPM() {
+		return fireMotor.getEncVelocity() * 600 / RobotMap.MotorConstants.ENCODER_TICKS_PER_ROTATION;
 	}
-	private double motorDeadzone(double power){
-		if(power == 0){
-			return 0;
-		}else{
-			return Math.signum(power) * ((1 - RobotMap.MotorConstants.SHOOTER_DEADZONE) * Math.abs(power) + RobotMap.MotorConstants.SHOOTER_DEADZONE);
-		}
+
+	private double motorDeadzone(double power) {
+		return Math.signum(power) * ((1 - RobotMap.MotorConstants.SHOOTER_DEADZONE) * Math.abs(power)
+				+ RobotMap.MotorConstants.SHOOTER_DEADZONE);
 	}
-    public void initDefaultCommand() {
-    }
+
+	public void initDefaultCommand() {
+	}
 }
