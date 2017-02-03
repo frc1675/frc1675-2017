6e03be685b33a5bfa63edb8a201eaa6a5f67e28e
Purple Door <purpledoor420@team1675@gmail.com>
2017-02-02 19:00:23 -0600

    cleaned up TankDrive Command (Jacob)

--- src/org/usfirst/frc/team1675/robot/commands/drive/TankDrive.java
+++ src/org/usfirst/frc/team1675/robot/commands/drive/TankDrive.java
@@ -13,33 +13,23 @@ public class TankDrive extends Command {
 		requires(Robot.driveBase);
 	}
 
-	// Called just before this Command runs the first time
 	protected void initialize() {
 	}
 
-	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		double leftPower = Robot.oi.getDriverLeftYAxis();
-		double rightPower = Robot.oi.getDriverRightYAxis();
-
-		Robot.driveBase.setLeftMotors(leftPower);
-		Robot.driveBase.setRightMotors(rightPower);
-
+		Robot.driveBase.setLeftMotors(Robot.oi.getDriverLeftYAxis());
+		Robot.driveBase.setRightMotors(Robot.oi.getDriverRightYAxis());
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
 		return false;
 	}
 
-	// Called once after isFinished returns true
 	protected void end() {
 		Robot.driveBase.setLeftMotors(0);
 		Robot.driveBase.setRightMotors(0);
 	}
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
 	protected void interrupted() {
 		end();
 	}
