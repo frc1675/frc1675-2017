260d16c931b225a1b9343f2a540097eb4fb2d8d1
nasalaptop <team1675@team1675.net>
2017-02-04 09:14:19 -0600

    Emiliano, Tara, and Simone made the shifter

--- src/org/usfirst/frc/team1675/robot/commands/drive/ShiftHigh.java
+++ src/org/usfirst/frc/team1675/robot/commands/drive/ShiftHigh.java
@@ -33,10 +33,12 @@ public class ShiftHigh extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveBase.stopShifter();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
--- src/org/usfirst/frc/team1675/robot/commands/drive/ShiftLow.java
+++ src/org/usfirst/frc/team1675/robot/commands/drive/ShiftLow.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1675.robot.commands.drive;
 
 import org.usfirst.frc.team1675.robot.Robot;
+import org.usfirst.frc.team1675.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -9,32 +10,35 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class ShiftLow extends Command {
 
-    public ShiftLow() {
-    	requires (Robot.driveBase);
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	public ShiftLow() {
+		requires(Robot.driveBase);
+		this.setTimeout(RobotMap.DriveBaseConstants.SHIFTER_TIME);
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	Robot.driveBase.shiftLow();
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.driveBase.shiftLow();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.driveBase.stopShifter();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
