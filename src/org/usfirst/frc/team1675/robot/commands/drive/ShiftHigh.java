68f2e892d9905e7443eae6bf5956e818855ba52d
nasalaptop <team1675@team1675.net>
2017-02-04 09:19:33 -0600

    Emiliano, Tara, and Simone removed the commits in the shifters

--- src/org/usfirst/frc/team1675/robot/commands/drive/ShiftHigh.java
+++ src/org/usfirst/frc/team1675/robot/commands/drive/ShiftHigh.java
@@ -13,31 +13,23 @@ public class ShiftHigh extends Command {
     public ShiftHigh() {
     	requires (Robot.driveBase);
     	this.setTimeout(RobotMap.DriveBaseConstants.SHIFTER_TIME);
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.driveBase.shiftHigh();
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return isTimedOut();
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     	Robot.driveBase.stopShifter();
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     	end();
     }
